74534fc1b6a04186cb4723cae773167211d3022b
Jala Sh <jala.shikhaliyeva@gmail.com>
2024-08-07 11:01:26 +0400

    fix: Resolve React hydration error by ensuring client-side rendering for GaleryParagraph component
    - Added `useEffect` hook to set `isClient` state to true after initial render
    - Updated render logic to check for `ready` and `isClient` state
    - Ensured component renders only on the client side to prevent HTML mismatch

--- components/GaleryParagraph/index.jsx
+++ components/GaleryParagraph/index.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./style.module.css";
 import { useTranslation } from "next-i18next";
 
 function GaleryParagraph() {
   const { t, ready } = useTranslation();
+  const [isClient, setIsClient] = useState(false);
 
-  // Check if translations are ready
-  if (!ready) return null;
+  // Ensure this code runs only on the client side
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  // Check if translations are ready and if we are on the client side
+  if (!ready || !isClient) return null;
 
   return <div className={style.galleryTitle}>{t("qalereya")}</div>;
 }
